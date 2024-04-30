8c48c2a8c099ac74567b2ea1d70ac79604d18755
Esteban Puentes <rapuentes@uan.edu.co>
2024-04-29 22:55:16 -0500

    added ant design library and created lib/data to normalize the data obtained and created dashboardLogs Component this is the component that would have the logic to render the logs table and pass the call id to the detail call page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,11 +1,49 @@
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { unstable_noStore as noStore } from 'next/cache';
 import { NextResponse } from 'next/server';
 import { createClient } from "@/utils/supabase/server";
 
-export const fetchRecordings = async () => {
+
+interface fileResponseObject {
+    name: string,
+    id: string,
+    updated_at: string,
+    created_at: string,
+    last_accessed_at: string,
+    metadata: metadataObject,
+}
+interface normalizeRecordingsObject {
+    createdOn: string, //assigned from audio recording
+    direction: string,
+    toPhoneNumber: string,
+    fromPhoneNumber: string,
+    recordingUrl: string,
+    callLength: metadataObject,
+    cost: number,
+    status: string,
+    pathWayLogs: string,
+    transcript: string,
+    variables: string,
+    callId: string, //assigned from audio recording
+
+}
+
+interface metadataObject {
+    eTag: string,
+    size: number,
+    mimetype: string,
+    cacheControl: string,
+    lastModified: string,
+    contentLength: number,
+    httpStatusCode: number    
+}
+
+const supabase = createClient()
+
+export const fetchRecordings = async () : fileResponseObject => {
 
     noStore()
-    const supabase = createClient()
+    
     const { data, error } = await supabase
     .storage
     .from('leaping-audio-tech-interview')
@@ -13,3 +51,93 @@ export const fetchRecordings = async () => {
 
     return data
 }
+
+export const getFileUrls = async (bucket: string, path: string) : string => {
+
+    noStore()
+    
+    const { data, error } = await supabase
+    .storage
+    .from(bucket)
+    .getPublicUrl(path)
+    
+    const { publicUrl } = data
+    return publicUrl
+}
+
+export const getRecordingsNormalized = async () : normalizeRecordingsObject => {
+    const recordings = await fetchRecordings()
+    const fileUrls = await Promise.all(
+        recordings.map(async recording =>{
+            const url = await getFileUrls('leaping-audio-tech-interview', recording.name)
+            return {...recording, url}
+        })
+    )
+    const filesObj: normalizeRecordingsObject | null = {}
+    
+    const normalizeCalls = fileUrls.map(file=>{
+        if(file.metadata.mimetype.includes('audio/')){
+            const fileIndex: string = file.name.split('.')[0].split("").pop()
+            const toPhone: string = '+61 ' + Math.random().toString().split('.')[1].slice(0,9)
+            const fromPhone: string = '+61 ' + Math.random().toString().split('.')[1].slice(0,9)
+            if(!filesObj[fileIndex]){
+                filesObj[fileIndex] = {
+                    callIndex: fileIndex,
+                    createdOn: file.created_at,
+                    callId: file.id,
+                    toPhoneNumber: toPhone,
+                    fromPhoneNumber: fromPhone,
+                    recordingUrl: file.url,
+                    status: 'completed',
+                    pathWayLogs: 'view',
+                    variables: 'view',
+                    cost: 0,
+                    callLength: file.metadata.size,
+                    summary: 'N/A',
+                }
+            }else{
+                filesObj[fileIndex] = {
+                    ...filesObj[fileIndex], 
+                    createdOn: file.created_at,
+                    callId: file.id,
+                    toPhoneNumber: toPhone,
+                    fromPhoneNumber: fromPhone,
+                    recordingUrl: file.url,
+                    status: 'completed',
+                    pathWayLogs: 'view',
+                    variables: 'view'
+                }
+            }
+        }
+        if(file.metadata.mimetype.includes('text/')){
+            const fileIndex = file.name.split('.')[0].split("").pop()
+            if(!filesObj[fileIndex]){
+                filesObj[fileIndex] = {
+                    callIndex: fileIndex,
+                    transcript: file.url,
+                    cost: 0,
+                    callLength: file.metadata.size,
+                    summary: 'N/A',
+                }
+            }else{
+                filesObj[fileIndex] = {
+                    ...filesObj[fileIndex],
+                    transcript: file.url 
+                }
+            }
+            
+        }
+    })
+    
+	console.log("TCL: Object.values(filesObj)", Object.values(filesObj))
+    return Object.values(filesObj)
+}
+
+
+export const downloadBlobFileByName = async (name) => {
+    const {data: blob} = await supabase
+    .storage
+    .from('leaping-audio-tech-interview')
+    .download(name)
+}
+
--- components/molecules/DashboardLogs.tsx
+++ components/molecules/DashboardLogs.tsx
@@ -1,11 +1,82 @@
-import { fetchRecordings } from '../../app/lib/data.ts'
+import { getRecordingsNormalized } from '../../app/lib/data.ts'
+import { Table } from 'antd';
+
+const columns = [
+    {
+      title: 'Created On',
+      dataIndex: 'createdOn',
+      key: 'createdOn',
+    },
+    {
+      title: 'Direction',
+      dataIndex: 'direction',
+      key: 'direction',
+    },
+    {
+      title: 'To',
+      dataIndex: 'toPhoneNumber',
+      key: 'toPhoneNumber',
+    },
+    {
+      title: 'From',
+      dataIndex: 'fromPhoneNumber',
+      key: 'fromPhoneNumber',
+    },
+    {
+      title: 'Recording',
+      dataIndex: 'recordingUrl',
+      key: 'recordingUrl',
+    },
+    {
+      title: 'Call Length',
+      dataIndex: 'callLength',
+      key: 'callLength',
+    },
+    {
+      title: 'Cost',
+      dataIndex: 'cost',
+      key: 'cost',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Summary',
+      dataIndex: 'summary',
+      key: 'summary',
+    },
+    {
+      title: 'Pathway Logs',
+      dataIndex: 'pathWayLogs',
+      key: 'pathWayLogs',
+    },
+    {
+      title: 'Transcript',
+      dataIndex: 'transcript',
+      key: 'transcript',
+    },
+    {
+      title: 'Variables',
+      dataIndex: 'variables',
+      key: 'variables',
+    },
+    {
+      title: 'Call ID',
+      dataIndex: 'callId',
+      key: 'callId',
+    },
+  ];
 
 export default async function DashboardLogs() {
-    const recordings = await fetchRecordings()
-	console.log("TCL: DashboardLogs -> recordings", recordings)
+    const recordings = await getRecordingsNormalized()
+    // const fetchingContent = recordings.map(recording => {
+    //     const data =
+    // })
     return (
         <div>
-            <p>HOLIS</p>
+            <Table columns={columns} dataSource={recordings} />
         </div>
     )
 }
