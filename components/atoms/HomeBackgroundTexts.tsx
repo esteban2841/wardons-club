0b07364c4cb0fdbd4387a9ab8141cf5a392b2231
Esteban Puentes <rapuentes@uan.edu.co>
2024-07-09 16:36:01 -0500

    added Background Loader

--- components/atoms/HomeBackgroundTexts.tsx
+++ components/atoms/HomeBackgroundTexts.tsx
@@ -8,24 +8,69 @@ interface BackContainerProps {
     titleContent?: string;
     subtitleContent?: string;
     customAdjust?: string;
+    loaderPosition?: number;
 }
 interface ContainerProps {
     positionText: 'left' | 'right';
 }
+interface colorProps {
+    loaderPosition: number;
+}
 
 
-const BackgroundEthicContainer = styled.div<BackContainerProps>`
+const Container = styled.div`
     display: flex;
     flex-direction: row;
     position: relative;
     box-sizing: border-box;
     width: 100%;
     align-items: center;
+    justify-content: center;
+    
+    @media (max-width: 768px){
+        
+    }
+    `
+const BackgroundEthicContainer = styled.div<BackContainerProps>`
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100vh;
+    z-index: 2;
+    align-items: center;
     background-size: cover;
     background-repeat: no-repeat;
     background-position:  ${props => props.customAdjust ? `${props.customAdjust}%` : ''};
     justify-content: ${props => props.positionText ? (props.positionText) : ''};
     background-image:  url(${props => props.imageUrl ? (props.imageUrl) : ''});
+    
+    @media (max-width: 768px){
+        background-position:  30% 0%;
+        
+    }
+    `
+const BackgroundColorContainer = styled.div<colorProps>`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    z-index: 1;
+    background: linear-gradient(270deg, #ff0000, #00ff00, #0000ff);
+    background-size: 200% auto;
+    animation: Gradient 3s ease infinite;
+    
+    @keyframes Gradient {
+        0% {
+            background-position: 200% 0;
+        }
+        100% {
+            background-position: -200% 0;
+        }
+    }
+    
     @media (max-width: 768px){
         background-position:  30% 0%;
         
@@ -41,6 +86,7 @@ const TextContainer = styled.div<ContainerProps>`
     position: relative;
     padding: 60px;
     gap: 25px;
+
 `
 
 const Overlay = styled.div`
@@ -48,6 +94,7 @@ const Overlay = styled.div`
     flex-direction: column;
     position: absolute;
     width: 100%;
+    z-index: 3;
     height: 100%;
     background-color: rgb(0, 0, 0, 0.6);
 `
@@ -58,6 +105,7 @@ const WardonsTitle = styled.h1<ContainerProps>`
     font-style: bold;
     color: #fff;
     font-size: 66px;
+    z-index: 4;
     text-align: ${props => props.positionText};
     opacity: .7;
     @media (min-width: 1700px){
@@ -69,12 +117,13 @@ const WardonsTitle = styled.h1<ContainerProps>`
     }
     @media (max-width: 768px){
         font-size:30px;
-
+        
     }
     `
 const SubTitle = styled.h1<ContainerProps>`
     font-family: 'Futura Md BT', sans-serif;
     font-size: 24px;
+    z-index: 4;
     font-weight: 900;
     font-style: italic;
     opacity: .7;
@@ -89,14 +138,20 @@ const SubTitle = styled.h1<ContainerProps>`
     }
 `
 
-export const HomeBackgroundTexts = ({positionText, imageUrl, customAdjust, titleContent, subtitleContent}: BackContainerProps) => {
+export const HomeBackgroundTexts = ({loaderPosition ,positionText, imageUrl, customAdjust, titleContent, subtitleContent}: BackContainerProps) => {
   return (
-    <BackgroundEthicContainer customAdjust={customAdjust} positionText={positionText} imageUrl={imageUrl}>
-        <Overlay></Overlay>
-        <TextContainer positionText={positionText}>
-            <WardonsTitle positionText={positionText}>{titleContent}</WardonsTitle>
-            <SubTitle positionText={positionText}>{subtitleContent}</SubTitle>
-        </TextContainer>
-    </BackgroundEthicContainer>
+    <Container>
+
+        <BackgroundColorContainer loaderPosition={loaderPosition}>
+            <BackgroundEthicContainer  loaderPosition={loaderPosition} customAdjust={customAdjust} positionText={positionText} imageUrl={imageUrl}>
+            <Overlay></Overlay>
+                <TextContainer positionText={positionText}>
+                    <WardonsTitle positionText={positionText}>{titleContent}</WardonsTitle>
+                    <SubTitle positionText={positionText}>{subtitleContent}</SubTitle>
+                </TextContainer>
+            </BackgroundEthicContainer>
+        </BackgroundColorContainer>
+    </Container>
+
   )
 }
