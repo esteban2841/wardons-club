4109b02a4664d922ed8d52d20bb2617c791c80b5
Esteban Puentes <rapuentes@uan.edu.co>
2024-07-17 17:07:31 -0500

    google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,12 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.className} box-border`}>
       <Head>
         <title>{"Club de Baloncesto Wardons Villavicencio"}</title>
+        <meta name="google-site-verification" content="b8r5HBioWBEaeO4Tx7qBrvWStbPszmGeZ71emKznmG4" />
         <meta name="description" content="Informacion, ubicacion y noticias de nuestro club deportivo de baloncesto wardons, formacion de atletas desde temprana edad mini y juveniles Villavicencio, Meta." />
         <meta property="og:title" content="Escuela, academia, club de Baloncesto Wardons Villavicencio" />
         <meta property="og:description" content="Clases grupales de baloncesto (basketball) para niños y jovenes de todas las edades" />
         <meta property="og:image" content="https://hzahrfjtetaexlyfdecg.supabase.co/storage/v1/object/public/gallery/GALLERY_BANNER.png" /> {/* Reemplaza URL_DE_TU_IMAGEN con la URL de una imagen representativa */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="google-site-verification" content="b8r5HBioWBEaeO4Tx7qBrvWStbPszmGeZ71emKznmG4" />
       </Head>
       <GoogleAnalytics gaId="G-1L1M5FQ7ZZ" />
       <body className="bg-[#000] text-foreground">
